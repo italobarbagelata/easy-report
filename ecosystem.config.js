61128c826518de6e3f9acc3ba7397ff83718ed80
ItaloJeanfranco <italo.barbagelataji@laureate.mx>
2024-08-14 11:57:48 -0400

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    apps: [{
-      name: "easy-report",
-      script: "npm run start",
-      env: {
-        NODE_ENV: "dev",
-        NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
-      },
-      env_test: {
-        NODE_ENV: "test",
-      },
-      env_staging: {
-        NODE_ENV: "staging",
-      },
-      env_production: {
-        NODE_ENV: "prod",
-        NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
-      }
-    }]
-  }
+  apps: [{
+    name: "easy-report",
+    script: "npm run build && npm run start",
+    env: {
+      NODE_ENV: "development",
+      NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
+    },
+    env_test: {
+      NODE_ENV: "test",
+    },
+    env_staging: {
+      NODE_ENV: "staging",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
+    }
+  }]
+}
