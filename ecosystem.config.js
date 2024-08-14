0684a2c7e4197262b213cd3fef38ed4046239201
ItaloJeanfranco <italo.barbagelataji@laureate.mx>
2024-08-14 02:18:48 -0400

    env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [{
       name: "easy-report",
-      script: "npm run dev",
+      script: "npm run start",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "dev",
         NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
         NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
       },
@@ -14,7 +14,9 @@ module.exports = {
         NODE_ENV: "staging",
       },
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: "prod",
+        NEXT_PUBLIC_SUPABASE_URL: "https://ofhsozrjmzxtsbfoczsf.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maHNvenJqbXp4dHNiZm9jenNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzExNjYsImV4cCI6MjAzMjIwNzE2Nn0.VEtNniWw1HwVaFRi3KALMvqD1RLGTZ3cjrrxeNmhCdA"
       }
     }]
   }
