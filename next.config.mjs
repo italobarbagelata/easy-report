dae242eb8ee48dfe656258f4665f5e64f41870f1
ItaloJeanfranco <italo.barbagelataji@laureate.mx>
2024-09-21 14:23:39 -0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    distDir: "build",
-  };
-  
-  export default nextConfig;
-  
+  distDir: "build",
+};
+
+export default nextConfig;
